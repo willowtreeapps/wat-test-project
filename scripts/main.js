070db4ba5dd256b8c4ceaace75ca8044b613dec6
jasmin47 <alxsanborn@gmail.com>
2016-11-21 16:04:30 -0500

    add question prompt

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,24 @@ $.ajax({
         return 0.5 - Math.random()
       })
 
-        document.getElementById('employee-1-pic').src = people[0].url
-        document.getElementById('employee-2-pic').src = people[1].url
-        document.getElementById('employee-3-pic').src = people[2].url
-        document.getElementById('employee-4-pic').src = people[3].url
-        document.getElementById('employee-5-pic').src = people[4].url
+      var correctAnswer = [people[0], people[1], people[2], people[3], people[4]]
 
+      correctAnswer.sort(function(){
+        return 0.5 - Math.random()
+      })
 
-        $("#employee-1-name").append(`${people[0].name}`)
-        $("#employee-2-name").append(`${people[1].name}`)
-        $("#employee-3-name").append(`${people[2].name}`)
-        $("#employee-4-name").append(`${people[3].name}`)
-        $("#employee-5-name").append(`${people[4].name}`)
+      $("#question").append(`${correctAnswer[0].name}`)
+
+      document.getElementById('employee-1-pic').src = people[0].url
+      document.getElementById('employee-2-pic').src = people[1].url
+      document.getElementById('employee-3-pic').src = people[2].url
+      document.getElementById('employee-4-pic').src = people[3].url
+      document.getElementById('employee-5-pic').src = people[4].url
+
+      $("#employee-1-name").append(`${people[0].name}`)
+      $("#employee-2-name").append(`${people[1].name}`)
+      $("#employee-3-name").append(`${people[2].name}`)
+      $("#employee-4-name").append(`${people[3].name}`)
+      $("#employee-5-name").append(`${people[4].name}`)
     }
 });
