ed645178ca817207c87b5c03f691d90b9890f05d
jasmin47 <alxsanborn@gmail.com>
2016-11-21 16:23:20 -0500

    render game that refreshes after each turn

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+function renderNewNames(){
 $.ajax({
     url: 'http://namegame.willowtreemobile.com:2000',
     success: function(people) {
@@ -10,8 +11,16 @@ $.ajax({
       correctAnswer.sort(function(){
         return 0.5 - Math.random()
       })
+      var employees = document.getElementsByClassName('employee')
+        for (var employee = 0; employee < employees.length; employee++){
+          var overlay = employees[employee].children[0]
+          var name = employees[employee].children[0].children[0]
+          $(`#${overlay.id}`).addClass('hidden-overlay').removeClass('incorrect-guess')
+          $(`#${overlay.id}`).addClass('hidden-overlay').removeClass('correct-guess')
+          $(`#${name.id}`).addClass('hidden-name-text').removeClass('guessed-name-text')
+        }
 
-      $("#question").append(`${correctAnswer[0].name}`)
+      $("#question").empty().append(`${correctAnswer[0].name}`)
 
       document.getElementById('employee-1-pic').src = people[0].url
       document.getElementById('employee-2-pic').src = people[1].url
@@ -19,10 +28,11 @@ $.ajax({
       document.getElementById('employee-4-pic').src = people[3].url
       document.getElementById('employee-5-pic').src = people[4].url
 
-      $("#employee-1-name").append(`${people[0].name}`)
-      $("#employee-2-name").append(`${people[1].name}`)
-      $("#employee-3-name").append(`${people[2].name}`)
-      $("#employee-4-name").append(`${people[3].name}`)
-      $("#employee-5-name").append(`${people[4].name}`)
+      $("#employee-1-name").empty().append(`${people[0].name}`)
+      $("#employee-2-name").empty().append(`${people[1].name}`)
+      $("#employee-3-name").empty().append(`${people[2].name}`)
+      $("#employee-4-name").empty().append(`${people[3].name}`)
+      $("#employee-5-name").empty().append(`${people[4].name}`)
     }
-});
+  });
+};
