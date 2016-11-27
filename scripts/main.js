ab7381b7399c0fca7b51c030fcefb9d294b5c538
Ian Thorp <IanMThorp@gmail.com>
2016-11-26 19:17:12 -0800

    Get data from  people api

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,48 @@
-$.ajax({
-    url: 'http://namegame.willowtreemobile.com:2000',
-    success: function(people) {
-        console.log(people);
-    }
-});
+var peopleApi = (function() {
+
+  var getData = function(cb) {
+    return $.ajax({
+        url: 'http://namegame.willowtreemobile.com:2000',
+        success: function(people) {
+            cb(people)
+        }
+    });
+  }
+
+  return {
+    getData: getData
+  }
+
+}())
+
+var guessingGame = (function($, window, document) {
+
+  var init = function () {
+    cacheDom();
+  }
+
+  var $ui, $pictureContainer, $questionContainer;
+
+  var cacheDom = function () {
+    $ui = $('body');
+    $picturesContainer = $ui.find('#pictures-container');
+    $questionContainer = $ui.find('#question-container');
+  }
+
+  var getNames = function() {
+    var apiCall = peopleApi.getData( function(people) {
+      return people;
+    })
+
+    $.when(apiCall).done(function(response) {
+      list = response;
+      console.log(list)
+    })
+  }
+
+  $(function() {
+    init()
+    getNames()
+  })
+
+}(window.jQuery, window, document))
